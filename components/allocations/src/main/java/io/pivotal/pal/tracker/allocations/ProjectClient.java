2002cb20c419e44fd193022d371404278ba75dce
kamalkumar <kamalkumar.madhavan@gmail.com>
2019-11-19 11:00:53 +0530

    Configured the Hysterix

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,26 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
-
+    private ConcurrentHashMap<Long, ProjectInfo> concurrentProjectMap = new ConcurrentHashMap<>();
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations= restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
-
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo projectInfo =restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+        concurrentProjectMap.put(projectId,projectInfo);
+        return projectInfo;
+    }
+    public ProjectInfo getProjectFromCache(long projectId) {
+        return concurrentProjectMap.get(projectId);
     }
 }
